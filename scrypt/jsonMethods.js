21b14fefc85f8729bac3d0495c616848119833e7
Yurii <cheremha.yriy@gmail.com>
2026-08-24 12:33:06 +0300

    Finish work wiht json

--- scrypt/jsonMethods.js
+++ scrypt/jsonMethods.js
@@ -1,10 +1,33 @@
-let user = {
-  name: "Іван Іванов",
-  age: 35
+// let user = {
+//   name: "Іван Іванов",
+//   age: 35
+// };
+
+// let jsnStr = JSON.stringify(user);
+// console.log(jsnStr);
+
+// let newObj = JSON.parse(jsnStr);
+// console.log(newObj.name);
+
+
+// task 2
+
+let room = {
+  number: 23
 };
 
-let jsnStr = JSON.stringify(user);
-console.log(jsnStr);
+let meetup = {
+  title: "Конференція",
+  occupiedBy: [{name: "Іван"}, {name: "Аліса"}],
+  place: room
+};
 
-let newObj = JSON.parse(jsnStr);
-console.log(newObj.name);
+// циклічне посилання
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+console.log( JSON.stringify(meetup, function replacer(key, value) {
+  if(value == meetup && key != '')
+    return undefined;
+  return value;
+}));
